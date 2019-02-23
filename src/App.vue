030090d586e3655c38d0eb7f66778c294d25ac9c
徐宾 <xuzhibin212@gmail.com>
2019-02-23 17:23:59 +0800

    完善配置

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,56 @@
 <template>
   <div id="app">
     <keep-alive>
-      <router-view :key="isRouterAlive"  v-if="$route.meta.keepAlive"></router-view>
+      <router-view :key="isRouterAlive" v-if="$route.meta.keepAlive"></router-view>
     </keep-alive>
     <router-view :key="isRouterAlive" v-if="!$route.meta.keepAlive"></router-view>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'App',
-  provide() {
-    return {
-      reload:this.reload,//定义修改或者删除页面刷新方法
-    }
-  },
-  data() {
-    return {
-      isRouterAlive: true
-    }
-  },
-  methods: {
-    reload() {
-      this.isRouterAlive = false;
-      this.$nextTick(function () {
-        this.isRouterAlive = true;
-      })
+  export default {
+    name: 'App',
+    provide() {
+      return {
+        reload: this.reload,//定义修改或者删除页面刷新方法
+      }
+    },
+    data() {
+      return {
+        isRouterAlive: true
+      }
+    },
+    methods: {
+      reload() {
+        this.isRouterAlive = false;
+        this.$nextTick(function () {
+          this.isRouterAlive = true;
+        })
+      }
     }
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  /*text-align: center;*/
-  /*color: #2c3e50;*/
-  /*margin-top: 60px;*/
-}
+  body, div, dl, dt, dd, ul, ol, li, h1, h2, h3, h4, h5, h6, pre, code, form, fieldset, legend, input, textarea, p, blockquote, th, td, article, aside, details, figcaption, figure, footer, header, hgroup, menu, nav, section {
+    margin: 0;
+    padding: 0;
+  }
+
+  .clearfix:after {
+    clear: both;
+  }
+
+  ul, ol, li {
+    list-style: none;
+  }
+
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    /*text-align: center;*/
+    /*color: #2c3e50;*/
+    /*margin-top: 60px;*/
+  }
 </style>
